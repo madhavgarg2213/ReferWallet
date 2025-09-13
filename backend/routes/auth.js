2fd7c8cf69d1253ef21b8e2a22607187b8455563
Madhav garg <143776768+madhavgarg2213@users.noreply.github.com>
2025-09-13 12:28:49 +0530

    checking

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post("/check", (req, res) => {
   const { password } = req.body;
 
-  if (password === process.env.SITE_PASSWORD) {
+  if (password === 12341) {
     return res.json({ success: true });
   } else {
     return res.status(401).json({ success: false, msg: "Invalid password" });
